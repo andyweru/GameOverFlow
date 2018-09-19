fbc6c8b79e86720300162f66cea8be4db8b6b126
Andrew Waweru <andyweru@gmail.com>
2018-09-19 11:43:04 +0300

    configured models

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,14 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+
+login_mananger = LoginManager()
+login_mananger.session_protection = 'strong'
+login_mananger.login_view = 'auth.login'
 
 def create_app(config_name):
 
@@ -21,6 +26,7 @@ def create_app(config_name):
     # Initializing flask extensions 
     bootstrap.init_app(app)
     db.init_app(app)
+    login_mananger.init_app(app)
 
     # Will and the views and forms
 
--- app/models.py
+++ app/models.py
@@ -1,9 +1,17 @@
+from flask_login import UserMixin
 from . import db
+from . import login_mananger
+
+@login_mananger.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(255))
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    password_hash = db.Column(db.String(255))
 
     def __repr__(self):
         return f'User {self.username}'
