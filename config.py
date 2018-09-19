59736e964068c71905b5c78d2a6ce31f5728472e
Andrew Waweru <andyweru@gmail.com>
2018-09-19 11:19:53 +0300

    db uri and password

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wesh:password@localhost/gameoverflow'
 
 class ProdConfig(Config):
     pass
